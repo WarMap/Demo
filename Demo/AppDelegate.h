166c0b9534abdee48040ccdaf24cfe3f89f5a870
warmap <mapeng712@gmail.com>
2017-10-18 20:56:46 +0800

    init

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by warmap on 2017/10/18.
+//  Copyright © 2017年 warmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by warmap on 2017/10/18.
+//  Copyright © 2017年 warmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
